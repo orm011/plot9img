542c58101df2e30dc22fe2fedcff958fccec81a2
Oscar Moll <orm@csail.mit.edu>
2022-04-27 14:51:00 -0400

    format

--- plot9img/__init__.py
+++ plot9img/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 from .ggimg import ggimg, ggimg_toImage, ggimg_draw
--- plot9img/ggimg.py
+++ plot9img/ggimg.py
@@ -10,10 +10,11 @@ import matplotlib.pyplot as plt
 import io
 import PIL.Image
 import plotnine
-import pandas 
+import pandas
 import matplotlib.pyplot as plt
 
-def _theme_image(w : int, h : int, dpi=80) -> plotnine.theme:
+
+def _theme_image(w: int, h: int, dpi=80) -> plotnine.theme:
     """a theme with plotnine defaults for showing images without coordinates"""
     return theme(
         axis_line=element_blank(),
@@ -37,7 +38,13 @@ def _theme_image(w : int, h : int, dpi=80) -> plotnine.theme:
         dpi=dpi,
     )
 
-def ggimg(image : PIL.Image.Image, mapping : plotnine.aes = None, data: pandas.DataFrame = None, dpi=80) -> plotnine.ggplot:
+
+def ggimg(
+    image: PIL.Image.Image,
+    mapping: plotnine.aes = None,
+    data: pandas.DataFrame = None,
+    dpi=80,
+) -> plotnine.ggplot:
     w, h = image.size
     return (
         ggplot(mapping=mapping, data=data)
@@ -47,6 +54,7 @@ def ggimg(image : PIL.Image.Image, mapping : plotnine.aes = None, data: pandas.D
         + _theme_image(w, h, dpi=dpi)
     )
 
+
 def _add_image(f, im):
     w, h = im.size
     for ax in f.get_axes():
@@ -55,16 +63,16 @@ def _add_image(f, im):
         ax.set_ylim(-h, 0)
     return f
 
-def ggimg_draw(ggim : plotnine.ggplot) -> plt.Figure:
-    """ draws image object to figure. 
-    """
+
+def ggimg_draw(ggim: plotnine.ggplot) -> plt.Figure:
+    """draws image object to figure."""
     f = ggim.draw()
     f = _add_image(f, ggim.environment.eval("image"))
     return f
 
-def ggimg_toImage(gimg : plotnine.ggplot) -> PIL.Image.Image:
-    """ renders to PIL.Image
-    """
+
+def ggimg_toImage(gimg: plotnine.ggplot) -> PIL.Image.Image:
+    """renders to PIL.Image"""
     plt.ioff()
     try:
         buf = io.BytesIO()
